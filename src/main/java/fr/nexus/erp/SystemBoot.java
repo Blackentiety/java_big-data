63f651d8eb538d2c5654084cff72cd46a219b69a
lukas <lukas.boudinl@gmail.com>
2026-03-04 15:09:49 +0100

    SystemBoot.verifierDroits

--- src/main/java/fr/nexus/erp/SystemBoot.java
+++ src/main/java/fr/nexus/erp/SystemBoot.java
@@ -1,4 +1,13 @@
 package fr.nexus.erp;
 
 public class SystemBoot {
+    public boolean verifierDroits(int niveauAccreditation) {
+        if (niveauAccreditation >= 5) {
+            System.out.println("Accès accordé. Bienvenue, Administrateur Nexus.");
+            return true;
+        } else {
+            System.out.println("ACCÈS REFUSÉ : Niveau d'accréditation insuffisant (" + niveauAccreditation + "/5).");
+            return false;
+        }
+    }
 }
