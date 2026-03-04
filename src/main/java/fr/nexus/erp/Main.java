a73eb36537f5ec1ea53053a62a3dedb15efb0ee1
lukas <lukas.boudinl@gmail.com>
2026-03-04 15:38:40 +0100

    Main.main

--- src/main/java/fr/nexus/erp/Main.java
+++ src/main/java/fr/nexus/erp/Main.java
@@ -1,4 +1,22 @@
 package fr.nexus.erp;
 
 public class Main {
+    public static void main(String[] args) {
+        SystemBoot boot = new SystemBoot();
+        DataEngine engine = new DataEngine();
+        int niveau = 5;
+
+        // 1. Validation de la sécurité (Niveau 6 pour passer)
+        if (boot.verifierDroits(niveau)) {
+
+            // 2. Affichage des étapes
+            boot.afficherEtapes();
+
+            // 3. Lancement du moteur de données
+            engine.runPipeline();
+
+        } else {
+            System.out.println("Système verrouillé. Fin du programme.");
+        }
+    }
 }
